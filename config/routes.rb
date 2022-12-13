2330ad6f3cc6966372480094b69f4b6e610ffb7c
skyv26 <dev.aakashv@gmail.com>
2022-12-14 00:05:25 +0530

    follow the `standards` for rails development

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
   # Routes for Pages
   get 'about' => 'pages#about_us'
   get 'contact' => 'pages#contact_us'
   get 'privacy-policy' => 'pages#privacy_policy'
   get 'terms-and-conditions' => 'pages#terms_and_conditions'
-
-  root 'home#index'
 end
